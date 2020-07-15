c640ba94f73a2f246d0dda41341e3780b68f8584
hector <jhaoblitas@gmail.com>
2020-07-15 08:37:57 -0500

    cambio de ruta en server

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,5 +4,5 @@ echo "****************"
 echo "* Building jar!*"
 echo "****************"
 
-
-docker run -v /root/.m2:/root/.m2 -v $PWD/Login:/Login -w /Login maven:3-alpine "$@"
+RUTA=/home/hector/Desktop/jenkins/jenkins_home/workspace/login-backend
+docker run -v /root/.m2:/root/.m2 -v $RUTA/Login:/Login -w /Login maven:3-alpine "$@"
--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -4,6 +4,6 @@ echo "################"
 echo "*** Testing  ***"
 echo "################"
 
-
-docker run -v /root/.m2:/root/.m2 -v $PWD/Login:/Login -w /Login maven:3-alpine "$@"
+RUTA=/home/hector/Desktop/jenkins/jenkins_home/workspace/login-backend
+docker run -v /root/.m2:/root/.m2 -v $RUTA/Login:/Login -w /Login maven:3-alpine "$@"
 
