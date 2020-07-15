03ee3e7316ad462d7c05982a18c3f8f7312c336a
HECTOR ARIAS <jhaoblitas@gmail.com>
2020-07-15 18:10:15 -0500

    config jenkinsfile

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,6 +4,6 @@ echo "****************"
 echo "* Building jar!*"
 echo "****************"
    
-sudo chmod -R 777 workspace/
+sudo chmod -R 777 Login/
 RUTA=/home/hector/Desktop/jenkins/jenkins_home/workspace/login-backend
 docker run -v /root/.m2:/root/.m2 -v $RUTA/Login:/Login -w /Login maven:3-alpine "$@"
